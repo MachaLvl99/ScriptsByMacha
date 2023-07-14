7935d8d42869751f21cfcd6e61362146fc836197
Macha <108778614+MachaLvl99@users.noreply.github.com>
2023-07-14 05:41:52 -0700

    Personal ChatGPT data processors
    These scripts allow me to modify and process my personal data provided by OpenAI collected from ChatGPT into something useful.

---
+++ ChatGPT-Related/ExtractAndTokenizeData.py
@@ -0,0 +1,36 @@
+
+import json
+import nltk
+nltk.download('punkt')
+
+def TokenizeStringSimple(s):
+    return nltk.word_tokenize(s)
+
+def ExtractAndTokenizeConversations(InputFile, OutputFile):
+    with open(InputFile, 'r') as f:
+        data = json.load(f)
+
+    ExtractedMessages = []
+    for conversation in data:
+        for NodeId, NodeData in conversation["mapping"].items():
+            if NodeData is not None and "message" in NodeData and NodeData["message"] is not None and NodeData["message"]["content"] is not None and NodeData["message"]["content"]["content_type"] == "text":
+                try:
+                    TokenizedContent = TokenizeStringSimple(NodeData["message"]["content"]["parts"][0])
+                    ExtractedMessages.append({
+                        "id": NodeId,
+                        "role": NodeData["message"]["author"]["role"],
+                        "content": NodeData["message"]["content"]["parts"][0],
+                        "tokens": TokenizedContent,
+                        "parent": NodeData["parent"],
+                        "children": NodeData["children"],
+                    })
+                except IndexError:
+                    pass
+
+    with open(OutputFile, 'w') as f:
+        json.dump(ExtractedMessages, f, indent=4)
+
+# Usage:
+# ExtractAndTokenizeConversations("/path/to/input.file", "/path/to/output.file")
+# conversations.json input
+# tokenized_messages.json output
---
+++ ChatGPT-Related/FormatAndChunkData.py
@@ -0,0 +1,46 @@
+
+import json
+import nltk
+nltk.download('punkt')
+
+def TokenizeStringSimple(s):
+    return nltk.word_tokenize(s)
+
+def FormatAndChunkMessages(InputFile, OutputFile):
+    with open(InputFile, 'r') as f:
+        data = json.load(f)
+
+    BeginMessage = "[BEGIN_MESSAGE|{}]"
+    EndMessage = "[END_MESSAGE]"
+    BeginChunk = "[BEGIN_CHUNK|CONVERSATION_1|PART_{}]"
+    EndChunk = "[END_CHUNK|CONVERSATION_1|PART_{}]"
+    MaxTokensPerChunk = 4096
+
+    Chunks = []
+    CurrentChunk = []
+    CurrentTokens = 0
+
+    for message in data:
+        FormattedMessage = f"{BeginMessage.format(message['role'].upper())} {message['content']} {EndMessage}"
+        TokenizedMessage = TokenizeStringSimple(FormattedMessage)
+
+        if CurrentTokens + len(TokenizedMessage) > MaxTokensPerChunk:
+            CurrentChunk.append(EndChunk.format(len(Chunks) + 1))
+            Chunks.append(' '.join(CurrentChunk))
+            CurrentChunk = [BeginChunk.format(len(Chunks) + 2)]
+            CurrentTokens = len(CurrentChunk[0])
+
+        CurrentChunk.append(FormattedMessage)
+        CurrentTokens += len(TokenizedMessage)
+
+    if CurrentChunk:
+        CurrentChunk.append(EndChunk.format(len(Chunks) + 1))
+        Chunks.append(' '.join(CurrentChunk))
+
+    with open(OutputFile, 'w') as f:
+        json.dump(Chunks, f, indent=4)
+
+# Usage:
+# FormatAndChunkMessages("/path/to/input.file", "/path/to/output.file")
+# tokenized_messages.json input
+# formatted_chunks.json output
